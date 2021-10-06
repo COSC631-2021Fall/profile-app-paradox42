2f40c658ba4f17cb5d5ceef5a33c6224c2b9b65b
jiansongHe <jiansonghe@live.cn>
2021-10-06 14:28:45 -0400

    changed React Component into functions

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 
-class About extends Component {
-  render() {
-    if (this.props.data) {
-      var name = this.props.data.name;
-      var profilepic = "images/" + this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
+const About = (props) => {
+  if (props.data) {
+    var name = props.data.name;
+    var profilepic = "images/" + props.data.image;
+    var bio = props.data.bio;
+    var street = props.data.address.street;
+    var city = props.data.address.city;
+    var state = props.data.address.state;
+    var zip = props.data.address.zip;
+    var phone = props.data.phone;
+    var email = props.data.email;
+    var resumeDownload = props.data.resumedownload;
+  }
 
-    return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
-            <img
-              className="profile-pic"
-              src={profilepic}
-              alt="Jiansong's Profile Pic"
-            />
-          </div>
-          <div className="nine columns main-col">
-            <h2>About Me</h2>
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img
+            className="profile-pic"
+            src={profilepic}
+            alt="Jiansong's Profile Pic"
+          />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
 
-            <p>{bio}</p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <span>
+                  {street}
                   <br />
-                  <span>
-                    {street}
-                    <br />
-                    {city} {state}, {zip}
-                  </span>
-                  <br />
-                  <span>{email}</span>
-                </p>
-              </div>
+                  {city} {state}, {zip}
+                </span>
+                <br />
+                <span>{email}</span>
+              </p>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default About;
